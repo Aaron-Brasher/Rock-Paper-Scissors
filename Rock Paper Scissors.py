14f616f89abf8b55efd2fc2b294611415217c698
Aaron <80337179+WhiteKnight4369@users.noreply.github.com>
2022-11-01 01:57:26 -0500

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,15 +1,20 @@
 from random import randint
+#create a list of play options
 t = ("rock ", "paper ", "scissors ")
-computer = t[randint(0,2)]
 
+#assign a random play to the computer
+computer = t[randint(0,2)]
+#User name 
 user = input("enter your name? ")
 
 print("\nWell Hello, " + str(user) + " How are you \n")
 
 print("please pick one of the following. " )
 
+#set player to False
 player = False
 
+#Set player to true
 while player == False:
 
     player = input("rock, paper, scissors ")
@@ -32,8 +37,10 @@ while player == False:
         if computer == "rock":
             print("you lose " + str(user) + " rock crushes scissors")
         else:
-            print("you win " + str(user) + " cut paper")
+            print("you win " + str(user) + " scissors cut paper")
     else: 
         print("That's not a valid play... Please check your spelling")
+
+    #player was set to True, but we want it to be False so the loop continues
     player = False
     computer = t[randint(0,2)]
